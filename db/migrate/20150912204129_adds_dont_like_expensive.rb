24906a4511f3a4d118a6757372fc7fea34b0e4b5
Luis Pereira <luiscper@gmail.com>
2015-09-12 21:46:55 +0100

    Adds the don't like reason

---
+++ db/migrate/20150912204129_adds_dont_like_expensive.rb
@@ -0,0 +1,7 @@
+class AddsDontLikeExpensive < ActiveRecord::Migration
+  def change
+  	add_column :products, :dontlikeexpensive_id, :integer
+  	add_column :products, :dontlikesize_id, :integer
+  	add_column :products, :dontlikecolour_id, :integer
+  end
+end
